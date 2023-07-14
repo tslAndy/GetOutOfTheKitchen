e1bb3cc86d81853ddb6f2d5fadd108b9555e4db2
Ilona <venusflytrapd.d@gmail.com>
2023-07-14 09:44:47 +0300

    Samall changes

--- Assets/Scripts/Player/Weapons/SuperPistol.cs
+++ Assets/Scripts/Player/Weapons/SuperPistol.cs
@@ -2,23 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pistol : Weapon
+public class SuperPistol : Weapon
 {
     [SerializeField] private Projectile projectilePrefab;
     [SerializeField] private float fireRate, projectileSpeed;
 
     private float _lastShotTime;
 
+
     public override void Attack(Vector2 direction)
     {
+
         if (Time.time < _lastShotTime + fireRate)
             return;
 
         Projectile projectile = Instantiate(projectilePrefab, transform);
         projectile.SetVelocity(direction * projectileSpeed);
     }
-
-    public override void RotateWeapon(Vector2 direction)
+     public override void RotateWeapon(Vector2 direction)
     {
         transform.up = direction;
     }
--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -6,4 +6,6 @@ public abstract class Weapon : Item
 {
     public override ItemType GetItemType() => ItemType.Weapon;
     public abstract void Attack(Vector2 direction);
+
+    public abstract void RotateWeapon(Vector2 direction);
 }
